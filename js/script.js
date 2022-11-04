4868bd412d3c051150a7293ee9acc47e75248e26
Alvaro Carvalho de Lima <112179805+Alvaro044@users.noreply.github.com>
2022-11-04 17:44:16 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$('button').click(function() {
+    $('.alert').removeClass('hide');
+    $('.alert').addClass('show');
+    $('.alert').addClass('showAlert');
+    setTimeout(function(){
+        $('.alert').removeClass('show');
+        $('.alert').addClass('hide');
+    }, 5000);
+});
+$('.close-btn').click(function(){
+    $('.alert').removeClass('show');
+    $('.alert').addClass('hide');
+});
